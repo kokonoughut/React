376abae06caf52b5b8883e84ed6aa80595135925
utsab <utsabcarki@gmail.com>
2023-11-29 15:40:27 +0545

    used the attributes added in App.js

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -4,6 +4,11 @@ const FirstComponent = (abc) => {
       <p>For example</p>
       <h1>{abc.name}</h1>
       <h1>{abc.courseName}</h1>
+      <p>
+        {abc.details.name} is {abc.age} years old and has{" "}
+        {abc.details.bloodGroup} blood and lives in {abc.details.address} and is{" "}
+        {abc.isStudent ? "" : "not "} a student
+      </p>
     </div>
   );
 };
