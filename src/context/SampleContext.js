6b3a43d45172784cdbd8a0d3d3248c60d9f07f0e
utsab <utsabcarki@gmail.com>
2023-12-18 15:17:57 +0545

    created a sample reducer for usereducer

--- src/context/SampleContext.js
+++ src/context/SampleContext.js
@@ -1,8 +1,21 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const SampleContext = createContext();
 
+const sampleReducer = (state, action) => {
+  switch (action.type) {
+    case "Update Test":
+      state.test = action.payload;
+      return state;
+    default:
+      return state;
+  }
+};
+
 const SampleProvider = ({ children }) => {
+  const [samplestate, dispatch] = useReducer(sampleReducer, {
+    test: "sample test",
+  });
   return (
     <SampleContext.Provider value={{ test: "Sample text" }}>
       {children}
